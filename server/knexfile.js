d40eac9a757254756b447edb4f37c5648c1872bc
Matt Gardner <matthewlgardner@gmail.com>
2016-11-30 15:05:56 -0700

    add production to knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,4 +4,8 @@ module.exports = {
         client: 'pg',
         connection: 'postgres://localhost/redditRefactor'
     },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
+    },
 };
